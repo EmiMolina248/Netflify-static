e08e4c02597ee92291c5fdef4d07f3bc7af07b3a
Emi <eemolinapereyro@escuelasproa.edu.ar>
2022-06-29 00:18:36 -0300

    inicio de 1

--- routes/myRouter.js
+++ routes/myRouter.js
@@ -5,7 +5,7 @@ const express = require("express");
 const router = express.Router();
 
 //Defino rutas y acciones de respuesta
-router.route("/").get(myController.inicio);
+//router.route("/").get(myController.inicio);
 router.route("/login").get(adminController.vista).post(adminController.logine);
 router.route("/logout").get(adminController.logout);
 router.route("/subirPost").get(adminController.postear2);
--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 const dotenv = require("dotenv");
+const port = 3500;
+const http = require("http");
+
+http.createServer((req, res) => {
+  // Homepage
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("Welcome to the homepage!");
+    }
+    else if (req.url === "/about") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("Welcome to the about page!");
+      }
+    
+      // 404'd!
+      else {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("404 error! File not found.");
+      }
+    }).listen(port, "localhost");
+
+
 
 //Carga de variables de entorno
 dotenv.config({ path: "./config.env" });
@@ -19,11 +41,11 @@ mongoose
         console.log("Connected to database");
     });
 
-const port = 3500;
-app.get("/test",(req,res) =>{
- res.send("GENIOOOOO")
-});
-//Corremos el servidor en el puerto seleccionado
-app.listen(port, () => {
-    console.log(`Servidor corriendo en el puerto ${port} correctamente`);
-});
+
+// app.get("/test",(req,res) =>{
+//  res.send("GENIOOOOO")
+// });
+// //Corremos el servidor en el puerto seleccionado
+// app.listen(port, () => {
+//     console.log(`Servidor corriendo en el puerto ${port} correctamente`);
+// });
